01a8992cb326aebbce7f6aa02179ea88da18711d
private <155294690+aswinxd@users.noreply.github.com>
2024-11-05 21:19:03 +0530

    Update watermark.py

--- watermark.py
+++ watermark.py
@@ -20,7 +20,7 @@ tasks = {}
 async def add_text_watermark(input_file, output_file, watermark_text):
     command = [
         'ffmpeg', '-i', input_file,
-        '-vf', f"drawtext=text='{watermark_text}':fontcolor=white:fontsize=24:box=1:boxcolor=black@1.0:x=10:y=(h-text_h)/2",
+        '-vf', f"drawtext=text='{watermark_text}':fontcolor=white:fontsize=24",
         '-codec:a', 'copy', output_file
     ]
     try:
